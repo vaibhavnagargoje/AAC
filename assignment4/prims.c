543c1b8e4d39d61e742f29641e6d6d70a6b04af6
Vaibhav Nagargoje <92326795+vaibhavnagargoje@users.noreply.github.com>
2023-12-04 08:14:01 +0000

    prims algorithm

--- assignment4/prims.c
+++ assignment4/prims.c
@@ -1,70 +1,80 @@
-#include <stdio.h>
-#include <limits.h>
+#include<stdio.h>
+#include<limits.h>
 
 #define MAX_VERTICES 20
 
 int graph[MAX_VERTICES][MAX_VERTICES];
 
-int minKey(int key[], int mstSet[], int vertices) {
-    int min = INT_MAX, min_index;
+int minKey(int key[],int mstSet[], int vertices){
+    int min = INT_MAX,min_index;
 
-    for (int v = 0; v < vertices; v++) {
-        if (mstSet[v] == 0 && key[v] < min) {
-            min = key[v];
-            min_index = v;
+    for(int v=0;v<vertices; v++){
+        if(mstSet[v]==0&& key[v] <min){
+            min=key[v];
+            min_index=v;
+
         }
     }
-
     return min_index;
+
 }
 
-void printMST(int parent[], int vertices) {
+void printMST(int parent[], int vertices){
     printf("Edge \tWeight\n");
-    for (int i = 1; i < vertices; i++)
-        printf("%d - %d \t%d \n", parent[i], i, graph[i][parent[i]]);
+    for(int i =1;i<vertices;i++){
+        printf("%d-%d \t %d \n",parent[i],i,graph[i][parent[i]]);
+
+    }
+    
 }
 
-void primMST(int vertices) {
+void primsMST(int vertices){
     int parent[MAX_VERTICES];
     int key[MAX_VERTICES];
     int mstSet[MAX_VERTICES];
 
-    for (int i = 0; i < vertices; i++) {
-        key[i] = INT_MAX;
-        mstSet[i] = 0;
+    for(int i=0; i<vertices;i++){
+        key[i]=INT_MAX;
+        mstSet[i]=0;
     }
+    key[0]=0;
+    parent[0]=-1;
 
-    key[0] = 0;
-    parent[0] = -1;
 
-    for (int count = 0; count < vertices - 1; count++) {
-        int u = minKey(key, mstSet, vertices);
-        mstSet[u] = 1;
-
-        for (int v = 0; v < vertices; v++) {
-            if (graph[u][v] && mstSet[v] == 0 && graph[u][v] < key[v]) {
-                parent[v] = u;
-                key[v] = graph[u][v];
+    for (int count=0; count<vertices-1;count++){
+        int u = minKey(key,mstSet,vertices);
+        mstSet[u]=1;
+        for (int v = 0; v < vertices; v++)
+        {
+            if (graph[u][v]&& mstSet[v]==0 && graph[u][v]<key[v]){
+                parent[v]=u;
+                key[v]=u;
+                key[v]=graph[u][v];
             }
         }
+        
     }
-
-    printMST(parent, vertices);
+    printMST(parent,vertices);
 }
 
-int main() {
+
+
+
+
+
+int main(){
     int vertices, edges;
-    printf("Enter the number of vertices: ");
+    printf("enter the number of vertices; ");
     scanf("%d", &vertices);
 
-    printf("Enter the adjacency matrix for the graph:\n");
-    for (int i = 0; i < vertices; i++) {
-        for (int j = 0; j < vertices; j++) {
-            scanf("%d", &graph[i][j]);
+    printf("Enter the adjacency matrix for the graph: \n");
+
+    for(int i=0; i< vertices; i++){
+        for(int j=0; j<vertices; j++){
+            scanf("%d",&graph[i][j]);
         }
     }
-
-    primMST(vertices);
-
+    primsMST(vertices);
     return 0;
 }
+
